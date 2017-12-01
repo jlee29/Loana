dbf578e2f8d6ce2fa96ac1b4a7a2e72943a95397
Jaime Deverall <jaime1deverall@gmail.com>
2017-12-01 02:32:01 -0800

    change default font

--- Loana/Meta Files/AppDelegate.swift
+++ Loana/Meta Files/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         print(Session.shared)
+        UILabel.appearance().font = UIFont(name: "Avenir", size: 15)
         // Override point for customization after application launch.
         return true
     }
