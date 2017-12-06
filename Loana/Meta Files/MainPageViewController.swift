fc161f1773ee0d0379e5bd252ff1c73b9cbe3e53
jlee29 <jlee29@stanford.edu>
2017-12-06 01:59:53 -0800

    added not working container page view

---
+++ Loana/Meta Files/MainPageViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MainPageViewController.swift
+//  Loana
+//
+//  Created by Jiwoo Lee on 12/6/17.
+//  Copyright © 2017 Jiwoo Lee. All rights reserved.
+//
+
+import UIKit
+
+class MainPageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    var pages = [UIViewController]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        self.dataSource = self
+        
+        let p1: UIViewController! = storyboard?.instantiateViewController(withIdentifier: "id1")
+        let p2: UIViewController! = storyboard?.instantiateViewController(withIdentifier: "id2")
+        let p3: UIViewController! = storyboard?.instantiateViewController(withIdentifier: "id3")
+        
+        pages.append(p1)
+        pages.append(p2)
+        pages.append(p3)
+        
+        setViewControllers([p1], direction: UIPageViewControllerNavigationDirection.forward, animated: false, completion: nil)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController)-> UIViewController? {
+        
+        let cur = pages.index(of: viewController)!
+        
+        // if you prefer to NOT scroll circularly, simply add here:
+        // if cur == 0 { return nil }
+        
+        let prev = abs((cur - 1) % pages.count)
+        return pages[prev]
+        
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController)-> UIViewController? {
+        
+        let cur = pages.index(of: viewController)!
+        
+        // if you prefer to NOT scroll circularly, simply add here:
+        // if cur == (pages.count - 1) { return nil }
+        
+        let nxt = abs((cur + 1) % pages.count)
+        return pages[nxt]
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController)-> Int {
+        return pages.count
+    }
+}
