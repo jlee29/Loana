2d6006d0280c9bcb9fa2316e416ef6a7b1682554
jlee29 <jlee29@stanford.edu>
2017-12-06 05:47:57 -0800

    halfway bank

---
+++ Loana/LoggedIn/BankTab/LinkAccount2ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LinkAccount2ViewController.swift
+//  Loana
+//
+//  Created by Jiwoo Lee on 12/6/17.
+//  Copyright © 2017 Jiwoo Lee. All rights reserved.
+//
+
+import UIKit
+
+class LinkAccount2ViewController: UIViewController {
+    
+    var bankName: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
